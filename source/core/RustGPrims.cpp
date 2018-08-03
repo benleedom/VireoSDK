b471605d9dbcfc37a9e536fd4bbef8c05f98603f
Ben Leedom <africanswallow@gmail.com>
2018-08-02 22:20:27 -0500

    MakeRef is now a generic function.

--- source/core/RustGPrims.cpp
+++ source/core/RustGPrims.cpp
@@ -1,8 +1,12 @@
 #include "TypeDefiner.h"
+#include "ExecutionContext.h"
+#include "TypeAndDataManager.h"
+#include "TDCodecVia.h"
+#include "VirtualInstrument.h"
 
 namespace Vireo {
 
-VIREO_FUNCTION_SIGNATURE2(MakeRefInt32, Int32, Int32*)
+VIREO_FUNCTION_SIGNATURE2(MakeRefImpl, void, void*)
 {
     _Param(1) = _ParamPointer(0);
     return _NextInstruction();
@@ -16,7 +20,8 @@ VIREO_FUNCTION_SIGNATURE3(DerefAddInt32, Int32*, Int32*, Int32)
 }
 
 DEFINE_VIREO_BEGIN(RustGPrims)
-    DEFINE_VIREO_FUNCTION_CUSTOM(MakeRefInt32, MakeRefInt32, "p(i(Int32) o(ptr(Int32)))")
+    DEFINE_VIREO_TYPE(GenericMakeRef, "p(i(*) o(ptr(*)))")
+    DEFINE_VIREO_FUNCTION_CUSTOM(MakeRef, MakeRefImpl, "GenericMakeRef")
     DEFINE_VIREO_FUNCTION_CUSTOM(DerefAddInt32, DerefAddInt32, "p(i(ptr(Int32)) i(ptr(Int32)) o(Int32))")
 DEFINE_VIREO_END()
 
